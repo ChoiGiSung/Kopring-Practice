6aab39d50d97fd55011f8cd0817ba6d00e1b6e97
gisungchoi <chlrltjd159@naver.com>
2021-12-19 15:50:19 +0900

    feat : init

---
+++ src/main/kotlin/com/kopring/coco/CocoApplication.kt
@@ -0,0 +1,11 @@
+package com.kopring.coco
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CocoApplication
+
+fun main(args: Array<String>) {
+	runApplication<CocoApplication>(*args)
+}
